05bffb63850c137452522da20b384beb585516fd
Lukas Gründer <l.gruender@hotmail.de>
2025-05-02 16:08:35 +0200

    feat(tasks): load todays tasks into an array

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ ram_path = "/home/ceadeus/Main/Organization/ObsidianVaults/Introspection_und_Org
 
 if __name__ == "__main__":
     with open(ram_path, 'r') as file:
+        tasks = []
         for line in file:
             line = line.strip()
+            if (line == ""):
+                continue
             if (line.startswith("#") and "01.05.2025" not in line):
                 break
+            if (not line.startswith("#")):
+                tasks.append(line)
             print(line)
