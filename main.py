0057339ac2a61b94bc2a27dd57afcfc155bc7be7
Lukas Gründer <l.gruender@hotmail.de>
2025-05-13 19:38:20 +0200

    feat(show): show updated ram list after check or delete

--- main.py
+++ main.py
@@ -151,6 +151,7 @@ if __name__ == "__main__":
                     break
         write_lines_to_file(lines)
         print("Deleted selected task" + ("" if len(tasks) == 1 else "s"))
+        reload_and_show_all()
 
     if args.mode == 'del':
         valid_mode = True
@@ -182,6 +183,7 @@ if __name__ == "__main__":
                     else:
                         lines[idx] = line.replace("[x]", "[ ]")
         write_lines_to_file(lines)
+        reload_and_show_all()
 
     if args.mode == 'check':
         valid_mode = True
